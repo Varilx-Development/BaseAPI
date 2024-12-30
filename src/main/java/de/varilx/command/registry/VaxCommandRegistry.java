d49f88009b8c04785efe1415eb693c9d8aa426a2
Shadow <nicolafabian75@gmail.com>
2024-12-30 10:01:14 +0100

    Refactor command registration and remove unused plugin field
    Replaced direct map insertion in `registerCommand` with a call to `registerServerCommand` to centralize command registration logic. Removed the unused `plugin` field for cleanup and improved clarity. These changes simplify the registry and ensure commands are properly tracked and synchronized.

--- src/main/java/de/varilx/command/registry/VaxCommandRegistry.java
+++ src/main/java/de/varilx/command/registry/VaxCommandRegistry.java
@@ -1,7 +1,5 @@
 package de.varilx.command.registry;
 
-
-import de.varilx.BaseAPI;
 import de.varilx.command.VaxCommand;
 import lombok.AccessLevel;
 import lombok.SneakyThrows;
@@ -9,7 +7,6 @@ import lombok.experimental.FieldDefaults;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandMap;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -17,16 +14,14 @@ import java.util.concurrent.ConcurrentHashMap;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class VaxCommandRegistry {
 
-    Plugin plugin;
     Map<String, VaxCommand> commands;
 
     public VaxCommandRegistry() {
-        this.plugin = BaseAPI.getBaseAPI().getPlugin();
         this.commands = new ConcurrentHashMap<>();
     }
 
     public void registerCommand(VaxCommand command) {
-        commands.put(command.getName(), command);
+        registerServerCommand(command);
     }
 
     public void unregisterCommand(String command) {
@@ -41,6 +36,7 @@ public class VaxCommandRegistry {
         CommandMap commandMap = Bukkit.getServer().getCommandMap();
         commandMap.register("", vaxCommand);
         commandMap.getKnownCommands().put(vaxCommand.getName(), vaxCommand);
+        commands.put(vaxCommand.getName(), vaxCommand);
         Bukkit.getOnlinePlayers().forEach(Player::updateCommands);
     }
 
